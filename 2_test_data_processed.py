9b44026492205bae1f9d834188a991ee1f87338c
Aakash Patel <aakashpatel2k18@gmail.com>
2020-06-07 12:25:11 +0530

    Update and rename test_data_processed.py to 2_test_data_processed.py

--- 2_test_data_processed.py
+++ 2_test_data_processed.py
@@ -6,9 +6,11 @@ import torch.optim as optim
 import torch.nn.functional as F
 import pickle
 
+PATH = "/home/smoke/Documents/Machine Learning/project/NeuACF/processed1/"
 
-rat_mat = np.genfromtxt("/home/smoke/Documents/Machine Learning/project/NeuACF/processed/UI_mat_3000_1000.csv",delimiter=',')
-positives = np.genfromtxt("/home/smoke/Documents/Machine Learning/project/NeuACF/dataset/amazon/amovie.test.rating",delimiter="\t",max_rows=3000)
+rat_mat = np.genfromtxt(PATH+"UI_new.csv",delimiter=',')
+
+positives = np.genfromtxt(PATH+"amovie.test.rating",delimiter="\t",max_rows=3000) ##taking only 3000 users
 print(len(positives))
 print(len(positives[0]))
 print(positives[0])
@@ -21,7 +23,7 @@ positives = np.delete(positives, [2,3], axis = 1)
 print(len(positives), len(positives[0]))
 print(positives[0])
 c=list()
-for i in range(len(positives)):
+for i in range(len(positives)):  ### taking only 1000 items
   if positives[i][1]>=1000:
     c.append(i)
 positives = np.delete(positives,c,axis = 0)
@@ -40,7 +42,9 @@ for i in positives:
     print(i[0])
   test_negatives[i[0]] = l
 
-a_file = open("/home/smoke/Documents/Machine Learning/project/NeuACF/processed/test_neg.pkl", "wb")
+a_file = open(PATH+"test_neg.pkl", "wb")
 pickle.dump(test_negatives, a_file)
 a_file.close()
+
+
 
